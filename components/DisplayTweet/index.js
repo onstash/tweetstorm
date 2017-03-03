48dcd426875f73da0461d4ded1c01c764c55d745
Santosh Venkatraman <santosh.venk@gmail.com>
2017-03-03 22:51:36 +0530

    Adds DisplayTweet component

---
+++ components/DisplayTweet/index.js
@@ -0,0 +1,14 @@
+const React = require('react');
+
+module.exports = ({ tweet, index }) => {
+  if (tweet) {
+    return (
+      <div className="tweetstorm-tweet">
+        { `${tweet} (${index + 1}/n)` }
+      </div>
+    );
+  }
+  else {
+    return <div className="tweetstorm-tweet" />;
+  }
+};
