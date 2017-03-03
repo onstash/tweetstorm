7bf4edf557bcc586d3e12a8c0704a8ad3721ec59
Santosh Venkatraman <santosh.venk@gmail.com>
2017-03-04 01:08:44 +0530

    Fixes TweetStorm markup & associated scss

--- components/TweetStorm/index.js
+++ components/TweetStorm/index.js
@@ -23,16 +23,25 @@ class TweetStorm extends React.Component {
 
   render() {
     return (
-      <div className="tweetstorm-container">
-        <div className="tweetstorm-input">
-          <textarea
-            ref="tweet"
-            placeholder="Type your tweet here"
-            onChange={ this.handleChange.bind(this) }
-          />
-        </div>
-        <div className="tweetstorm-preview">
-          <DisplayTweets />
+      <div className="outer-container">
+        <h2 className="heading">TWEETSTORM GENERATOR</h2>
+        <div className="content">
+          <div className="sub-headings">
+            <h3 className="sub-heading">TWEET</h3>
+            <h3 className="sub-heading">TWEETSTORM</h3>
+          </div>
+          <div className="contents">
+            <div className="tweet">
+              <textarea
+                ref="tweet"
+                placeholder="Type your tweet here"
+                onChange={ this.handleChange.bind(this) }
+              />
+            </div>
+            <div className="tweetstorm">
+              <DisplayTweets />
+            </div>
+          </div>
         </div>
       </div>
     );
