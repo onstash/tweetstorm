bb56fecc72907612f5d254608e7704eb910974b2
Santosh Venkatraman <santosh.venk@gmail.com>
2017-03-07 18:05:15 +0530

    Adds gulp task for running tests

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 const gulp = require('gulp');
 const gutil = require('gulp-util');
 const eslint = require('gulp-eslint');
+const shell = require('gulp-shell');
 
-const { getAllJSFiles } = require('./gulp/helpers');
+const {
+  getAllJSFiles, getAllTestFiles, registerLintedGulpTasks
+} = require('./gulp/helpers');
 
 gulp.task('lint', () => {
   return gulp.src(getAllJSFiles())
@@ -10,4 +13,12 @@ gulp.task('lint', () => {
                useEslintrc: true
              }))
              .pipe(eslint.formatEach('compact', gutil.log));
+});
+
+const EXEC_TEST = 'exec-test';
+registerLintedGulpTasks(gulp)(EXEC_TEST, shell.task([
+  `./node_modules/.bin/babel-tape-runner ${getAllTestFiles().join(' ')} | ./node_modules/.bin/tap-colorize`,
+]));
+gulp.task('test', [EXEC_TEST], () => {
+  gulp.watch(getAllJSFiles(), [EXEC_TEST]);
 });
